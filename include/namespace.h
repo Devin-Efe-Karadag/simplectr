0b16476e79145e6ea7ee366101a8c70a9583e1a8
Devin Efe Karadag <114683781+Devin-Efe-Karadag@users.noreply.github.com>
2026-01-03 13:20:00 +0300

    set isolated container hostname

--- include/namespace.h
+++ include/namespace.h
@@ -2,3 +2,6 @@
 #define SIMPLE_NAMESPACE_H
 
 #include <sys/types.h>
+#define CHILD_STACK (1024 * 1024)
+
+pid_t namespace_clone(int (*entry)(void *), void *arg, void *stack);
--- src/namespace.c
+++ src/namespace.c
@@ -9,3 +9,10 @@
                  CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWIPC | CLONE_NEWCGROUP |
                      CLONE_NEWNET | SIGCHLD,
                  arg);
+}
+
+int namespace_prepare(const char *hostname) {
+    if (mount(NULL, "/", NULL, MS_REC | MS_PRIVATE, NULL))
+        return -1;
+    return sethostname(hostname, strlen(hostname));
+}
